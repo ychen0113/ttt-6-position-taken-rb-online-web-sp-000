40b903341f0162b754417336d9c814bc004f9682
Yiliang Chen <yiliangchen0113@hotmail.com>
2020-08-29 19:01:37 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,10 +3,10 @@ def position_taken?(board, index)
   board = []
   
  if (board[index] = " ") || (board[index] = "") || (board[index] = nil)
-   position_taken? == false
+   position_taken? = false
    elsif (board[index] = "X")
-     position_taken? == true 
+     position_taken? = true 
      elsif (board[index] = "O")
-     position_taken? == true
+     position_taken? = true
 end
 end
