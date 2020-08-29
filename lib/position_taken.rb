7d2b29ec211c0a188d31036b7216f304dba790a6
Yiliang Chen <yiliangchen0113@hotmail.com>
2020-08-29 19:06:17 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,10 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  board = []
+  board = Array.new 
   
- if (board[index] = " ") || (board[index] = "") || (board[index] = nil)
+ if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
    false
-   elsif (board[index] = "X") || (board[index] = "O")
+   elsif (board[index] == "X") || (board[index] == "O")
    true 
 end
 end
